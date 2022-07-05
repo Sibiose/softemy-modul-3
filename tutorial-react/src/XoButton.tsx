9bcfc2a55a1186a2a7f538dd644e17f52ba9731d
Corneliu Maris <corneliu.maris@umwerk.com>
2022-07-05 19:56:55 +0300

    Added images

--- tutorial-react/src/XoButton.tsx
+++ tutorial-react/src/XoButton.tsx
@@ -13,22 +13,27 @@ export const XoButton = (props: {
         state.currentPlayer = state.currentPlayer === 'X' ? 'O' : 'X';
         props.setState({ ...state });
     }
-
+    let imgSrc = 'empty-image.svg';
+    if (props.state.board[props.index] === 'X') {
+        imgSrc = 'x-img.svg';
+    } else if (props.state.board[props.index] === 'O'){
+        imgSrc = 'o-img.svg'
+    }
     const background = props.state.board[props.index] !== '' ? 'yellow' : ''
     return (
-        <div 
-        onClick={handleClick}
-        style={{
-            background,
-            width: '40px',
-            height: '40px',
-            fontSize: '30px',
-            lineHeight: '40px',
-            textAlign: 'center',
-            borderStyle: 'solid',
-            borderColor: 'black'
-        }}>
-            {props.state.board[props.index]}
-        </div>
+        <img src={imgSrc}
+            onClick={handleClick}
+            style={{
+                background,
+                width: '40px',
+                height: '40px',
+                fontSize: '30px',
+                lineHeight: '40px',
+                textAlign: 'center',
+                borderStyle: 'solid',
+                borderColor: 'black'
+            }}>
+
+        </img>
     )
 }
